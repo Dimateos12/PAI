ad261f916f556d7e6cc32ccf4f5d9a6ae5ba77a9
Mateusz <mateusz.jablonski12@gmail.com>
2023-10-12 19:07:09 +0200

    Fix

---
+++ Data/PAIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PAI.Data
+{
+    public class PAIDbContext: IdentityDbContext
+    {
+        public PAIDbContext(DbContextOptions<PAIDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
