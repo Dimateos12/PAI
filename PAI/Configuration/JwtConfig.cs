d3516c42b462ed769f32afc5e5af0e2a112654c7
Mateusz <mateusz.jablonski12@gmail.com>
2023-10-13 15:48:46 +0200

    Add new model DTO RegistrationRequestDTO.cs and JwtConfig.cs

---
+++ PAI/Configuration/JwtConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace PAI.Configuration;
+
+public class JwtConfig
+{
+    public string Secret { get; set; }   
+}
---
+++ PAI/Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PAI.Common.ModelsDTO;
+
+namespace PAI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthenticationController(IConfiguration configuration,
+        UserManager<IdentityUser> userManager)
+    {
+        _configuration = configuration;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] RegistrationRequestDTO request)
+    {
+        //TODO
+        return  Ok();
+
+    }
+    
+    
+}
